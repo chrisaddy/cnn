100f9642d623116b493936cb30cc6f4bbb260712
chrisaddy <chris.william.addy@gmail.com>
2018-06-10 08:36:45 -0400

    add custom 404

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ Markdown(app)
 
 nav = Nav()
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route("/")
 def index():
 	return render_template('index.html')
